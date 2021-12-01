306921cefbe419cf200420551c8fb852ba5e766f
iamagf <agf0710@gmail.com>
2021-12-01 23:09:17 +0100

    Experience

---
+++ src/react/lower/Experience.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { experienceData } from "./jsonData/experienceInfo";
+
+class Experience extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div class='Experience'>
+                <div class='lower-experience'>
+                    <div class='lower-experience-title'>
+                        Experience
+                    </div>
+                    <div class='lower-experience-content'>
+                        <table>
+                        {
+                                experienceData.map((value) =>{
+                                    return (
+                                        <div>
+                                        <tr>
+                                            <td>
+                                                <div class='date'>{value.enter}/{value.exit}</div>
+                                            </td>
+                                            <td>
+                                                <strong>
+                                                    {value.title}
+                                                </strong> (<em><a target='_blank' href={value.link}>{value.center}</a>, {value.location}</em>)
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td></td>
+                                            <ul>
+                                                {value.content.map( ( tasks ) => {
+                                                    return (<li>{tasks}</li>)
+                                                })}
+                                            </ul>
+                                        </tr>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </table>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Experience;
