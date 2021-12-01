53d1401b2b41e512b0dbc8201a39b42b7cf44cb2
iamagf <agf0710@gmail.com>
2021-12-01 23:09:24 +0100

    Prizes

---
+++ src/react/lower/Prizes.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { prizesData } from './jsonData/prizesInfo';
+
+
+class Prizes extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div class='Prizes'>
+                <div class='lower-prizes'>
+                    <div class='lower-prizes-title'>
+                        Prizes
+                    </div>
+                    <div class='lower-prizes-content'>
+                        <table>
+                            {
+                                prizesData.map((value) => {
+                                    return (
+                                        <div>
+                                            <tr>
+                                                <td>
+                                                    <div class='date'>{value.date}</div>
+                                                </td>
+                                                <td>
+                                                    <strong>
+                                                        {value.position}
+                                                    </strong> (<em><a target='_blank' href={value.link}>{value.contest}</a></em>)
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td></td>
+                                                <td>{value.detail}
+                                                </td>
+                                            </tr>
+                                        </div>
+                                    )
+                                })
+                            }
+                            
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+        );
+    }
+}
+
+export default Prizes;
