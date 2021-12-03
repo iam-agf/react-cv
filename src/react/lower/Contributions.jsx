3ae2be5b20d3286bfeac333cb55f024a3452ab9e
iamagf <agf0710@gmail.com>
2021-12-03 02:09:49 +0100

    Added contributions to page

---
+++ src/react/lower/Contributions.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { contributionsData } from './jsonData/contributionsInfo';
+
+class Contributions extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div class='lower-contributions'>
+                <div class='lower-contributions-title'>
+                    Open Source <div class='lower-contributions-title-subtitle'>(Clickable titles)</div> 
+                </div>
+                <div class='lower-contributions-content'>
+                    <table>
+                        {
+                            contributionsData.map((value) => {
+                                return (
+                                    <div>
+                                        <tr>
+                                        <td>
+                                            <strong><a target='_blank' href={value.link}>
+                                                {value.name}
+                                            </a></strong>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                            {value.details}
+                                    </tr>
+                                    </div>
+                                )
+                            })
+                        }
+                    </table>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Contributions;
---
+++ src/react/lower/jsonData/contributionsInfo.js
@@ -0,0 +1,11 @@
+export const contributionsData = [
+    {
+        "name": "python-docs-es",
+        "details": "Help in the translation from English to Spanish of the Official Python Website with usage of Poedit.",
+        "link": "https://github.com/python/python-docs-es/issues?q=is%3Aissue+is%3Aclosed+assignee%3Aiam-agf"
+    },     {
+        "name": "python-docs-es",
+        "details": "Reviewer of pull requests of translations from English to Spanish of the Official Python Website.",
+        "link": "https://github.com/python/python-docs-es/pulls?q=is%3Apr+is%3Aclosed+reviewed-by%3Aiam-agf+"
+    }
+]
