fbc1aba9c631c8110a0d5373bf0564303bb2a0bf
iamagf <agf0710@gmail.com>
2021-12-03 02:37:34 +0100

    Better prize description

--- src/react/lower/jsonData/prizesInfo.js
+++ src/react/lower/jsonData/prizesInfo.js
@@ -4,35 +4,35 @@ export const prizesData = [
         "position": "22nd Place",
         "contest": "ACM-ICPC México and Central America Finals",
         "location": "México City, México, México and Central America",
-        "detail": "Celebrated in the Computer School of the IPN.<br /> Team: Elias Tuyú Alamilla, Elsy Pinzón Vivas, Adán Vera Euán; Participation as coach.",
+        "detail": "Participation as coach training the team that participated in this edition. Teaching of Data Structures and Algorithms for the contest.",
         "link": ""
     }, {
         "date": 2015,
         "position": "Honorable Mention",
         "contest": "Mathematical Contest in Modeling by COMAP",
         "location": "Online, International",
-        "detail": "Selected between 7636 teams.<br /> Team: Antonio González Fernández, Samuel Chablé Naal. Coach: Erik Ávila Vales.",
+        "detail": "Contest in solving a real life problem modeling it with usage of Mathematics and other sciences. Selected between 7636 teams.",
         "link": "https://www.comap-math.com/mcm/2015Certs/41823.pdf"
     }, {
         "date": "2014",
         "position": "University Merit Distinction",
         "contest": "IX University Merit Event",
         "location": "Yucatán, México",
-        "detail": "Best generation student undergraduate students in the Bachelor's Degree in Mathematics.",
+        "detail": "Best undergraduate student of the year. This prize can be won once per student and it's given one per year in the Bachelor's Degree in Mathematics.",
         "link": "https://www.facebook.com/pg/NFUUADY/photos/?tab=album&album_id=846155005417074&ref=page_internal"
     }, {
         "date": "2011",
         "position": "Silver Medal",
         "contest": "XXV Mexican Mathematical Olympiad",
         "location": "San Luis Potosí, México",
-        "detail": "Over 196 contestants preselected from each of their states.",
+        "detail": "Mathematical contest that evaluates the creativity applying your knowledge in Number Theory, Geometry, Combinatorics and Algebra. Global position 25 of 196 contestants all over the country.",
         "link": "https://www.ommenlinea.org/wp-content/uploads/engargolados/engargolado11.pdf"
     }, {
         "date": "2010",
         "position": "Bronze Medal",
         "contest": "XXIV Mexican Mathematical Olympiad",
         "location": "Baja California, México",
-        "detail": "Over 196 contestants preselected from each of their states.",
+        "detail": "Mathematical contest that evaluates the creativity applying your knowledge in Number Theory, Geometry, Combinatorics and Algebra. Global position 84 of 196 contestants all over the country.",
         "link": "https://www.ommenlinea.org/wp-content/uploads/engargolados/engargolado10.pdf"
     }
 ]
