e1b927a86aa50887fcbe0c694eb4742cd1777935
iamagf <61362029+iam-agf@users.noreply.github.com>
2022-01-03 13:19:02 -0600

    Minor changes

--- src/react/lower/jsonData/prizesInfo.js
+++ src/react/lower/jsonData/prizesInfo.js
@@ -5,34 +5,38 @@ export const prizesData = [
         "contest": "ACM-ICPC México and Central America Finals",
         "location": "México City, México, México and Central America",
         "detail": "Contest that evaluate skills in Data Structures and Algorithms to solve by teams a series of computational problems.",
-        "link": ""
+        "linkPrize": "https://iam-agf.github.io/cv.io/2015_acm_icpc_24.pdf"
     }, {
         "date": "2015",
         "position": "Honorable Mention",
         "contest": "Mathematical Contest in Modeling by COMAP",
         "location": "Online, International",
         "detail": "Contest focused on modeling and solving a real life problem applying Mathematics and other sciences. Honorable mention over 7636 teams.",
-        "link": "https://www.comap-math.com/mcm/2015Certs/41823.pdf"
+        "link": "https://www.comap-math.com/mcm/2015Certs/41823.pdf",
+        "linkPrize": "https://iam-agf.github.io/cv.io/2015_02_MCM.pdf"
     }, {
         "date": "2014",
         "position": "University Merit Distinction",
         "contest": "IX University Merit Event",
         "location": "Yucatán, México",
         "detail": "Best undergraduate student of the year. This prize can be won once per student and it's given one per year in the Bachelor's Degree in Mathematics.",
-        "link": "https://www.facebook.com/pg/NFUUADY/photos/?tab=album&album_id=846155005417074&ref=page_internal"
+        "link": "https://www.facebook.com/pg/NFUUADY/photos/?tab=album&album_id=846155005417074&ref=page_internal",
+        "linkPrize": "https://iam-agf.github.io/cv.io/2014_11_07_university_merit.pdf"
     }, {
         "date": "2011",
         "position": "Silver Medal",
         "contest": "XXV Mexican Mathematical Olympiad",
         "location": "San Luis Potosí, México",
         "detail": "Mathematical contest that evaluates the creativity applying your knowledge in Number Theory, Geometry, Combinatorics and Algebra. Global position 25 of 196 contestants all over the country.",
-        "link": "https://www.ommenlinea.org/wp-content/uploads/engargolados/engargolado11.pdf"
+        "link": "https://www.ommenlinea.org/wp-content/uploads/engargolados/engargolado11.pdf#page=57",
+        "linkPrize": "https://iam-agf.github.io/cv.io/2010_11_27_omm_third.pdf"
     }, {
         "date": "2010",
         "position": "Bronze Medal",
         "contest": "XXIV Mexican Mathematical Olympiad",
         "location": "Baja California, México",
         "detail": "Mathematical contest that evaluates the creativity applying your knowledge in Number Theory, Geometry, Combinatorics and Algebra. Global position 84 of 196 contestants all over the country.",
-        "link": "https://www.ommenlinea.org/wp-content/uploads/engargolados/engargolado10.pdf"
+        "link": "https://www.ommenlinea.org/wp-content/uploads/engargolados/engargolado10.pdf#page=54",
+        "linkPrize": "https://iam-agf.github.io/cv.io/2011_11_19_Const_omm_second.pdf"
     }
 ]
